1a855e9aa7df8599ec0af458130a96de582fe1d7
Kaspars Bulins <kasparsb@hotmail.com>
2019-03-26 17:38:32 +0000

    Navigation prev/next formatters

--- js/dom/dateSwitchEl.js
+++ js/dom/dateSwitchEl.js
@@ -1,20 +1,32 @@
-function navPrev() {
+function navPrev(props) {
     var el = document.createElement('a');
     el.className = 'calendar__nav calendar__nav--prev';
 
+    var c = props.get('navPrevFormatter', defaultNavPrevFormatter)()
+
+    /**
+     * Ja formatter atgriež string, tad veidojam document.createTextNode
+     * pretējā gadījumā vienkārši appendChild
+     */
     el.appendChild(
-        document.createTextNode('<')
+        typeof c == 'string' ? document.createTextNode(c) : c
     );
 
     return el;
 }
 
-function navNext() {
+function navNext(props) {
     var el = document.createElement('a');
     el.className = 'calendar__nav calendar__nav--next';
 
+    var c = props.get('navNextFormatter', defaultNavNextFormatter)()
+
+    /**
+     * Ja formatter atgriež string, tad veidojam document.createTextNode
+     * pretējā gadījumā vienkārši appendChild
+     */
     el.appendChild(
-        document.createTextNode('>')
+        typeof c == 'string' ? document.createTextNode(c) : c
     );
 
     return el;
@@ -37,14 +49,22 @@ function defaultFullDateFormatter(date) {
     return date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate()
 }
 
+function defaultNavPrevFormatter() {
+    return '<';
+}
+
+function defaultNavNextFormatter() {
+    return '>';
+}
+
 function dateSwitchDomElement(date, props) {
     
     this.props = props;
 
     this.el = document.createElement('div');
     
-    this.navPrev = navPrev();
-    this.navNext = navNext();
+    this.navPrev = navPrev(this.props);
+    this.navNext = navNext(this.props);
     this.dateCaptionTextNode = dateCaptionTextNode(date, this.props)
     this.dateCaption = dateCaption(date, this.dateCaptionTextNode);
     
