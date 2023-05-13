0d51b03d4512757ccc62f8c3057ba1554ab0d195
Kaspars Bulins <kasparsb@hotmail.com>
2023-05-13 21:14:51 +0300

    iztirits index fails

--- js/calendar.js
+++ js/calendar.js
@@ -1,25 +1,5 @@
 import dom from './dom/render2';
 
-import periodStructure from './periodStructure';
-import {
-    week as weekPeriod,
-    month as monthPeriod,
-    monthWithFullWeeks as monthWithFullWeeksPeriod
-} from './createPeriod';
-
-
 export {
-    weekPeriod,
-    monthPeriod,
-    monthWithFullWeeksPeriod,
-
-    periodStructure,
-
     dom
 }
-
-// export default {
-//     //dom: domRender,
-
-//     periodStructure: periodStructure
-// }
