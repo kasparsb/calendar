b5708ace25aa22bda7e6e6107454e1046a657587
root <root@note2.my.domain>
2024-03-08 21:16:24 +0000

    State url example

--- examples/calendar-state.php
+++ examples/calendar-state.php
@@ -2,6 +2,8 @@
 
 header('content-type: application/json');
 
+$isV2 = intval(filter_input(INPUT_GET, 'v')) == 2;
+
 $from = strtotime(filter_input(INPUT_GET, 'from'));
 $till = strtotime(filter_input(INPUT_GET, 'till'));
 
@@ -10,11 +12,21 @@ $current = $from;
 $state = [];
 
 while ($current <= $till) {
-    $state[date('Y-m-d', $current)] = [
-        'disabled' => rand(0, 1) ? true : false,
-        'html' => date('d', $current).' <b>asd</b>',
-        'cssClass' => 'random-'.rand(1, 4).' asdasd',
-    ];
+
+    if ($isV2) {
+        $state[date('Y-m-d', $current)] = [
+            'disabled' => rand(0, 1) ? true : false,
+            'html' => date('d', $current).' <b>V2</b>',
+            'cssClass' => 'random-'.rand(1, 4).' x',
+        ];
+    }
+    else {
+        $state[date('Y-m-d', $current)] = [
+            'disabled' => rand(0, 1) ? true : false,
+            'html' => date('d', $current).' <b>asd</b>',
+            'cssClass' => 'random-'.rand(1, 4).' asdasd',
+        ];
+    }
     // Add day
     $current += 60 * 60 * 24;
 }
